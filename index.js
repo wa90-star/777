de955d335a61b7abfd8b6baa54f99b9383571b7e
wa90-star <w.a.90@mail.de>
2026-08-30 21:15:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const server = http.createServer((req, res) => {
 
   fs.readFile(filePath, (err, data) => {
     if (err) {
+      console.error(err);
       res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
       res.end("Fehler beim Laden");
       return;
