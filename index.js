f308b2a29694fdf17d682aea3178e06357c7fa52
wa90-star <w.a.90@mail.de>
2026-08-30 21:12:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("777 is running");
+  const filePath = path.join(__dirname, "öffentlich", "index.html");
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end("Fehler beim Laden");
+      return;
+    }
+
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.end(data);
+  });
 });
 
 server.listen(port, "0.0.0.0");
